8cb7472f4bdb15bc7ba50d39f314f98555d947a1
Yamil Esteban <estebany035@gmail.com>
2023-09-21 20:13:06 -0500

    list of contacts (phase 1)

--- core-components/App.js
+++ core-components/App.js
@@ -1,28 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, TextInput, View, Button, FlatList } from "react-native";
 
 export default function App() {
+  const [contact, setContact] = useState("");
+  const [contacts, setContacts] = useState([]);
+
+  const addContactHandler = () => {
+    if (contact) {
+      // Agregar el nuevo contacto a la lista de contactos
+      setContacts([...contacts, { name: contact }]);
+      // Limpiar el campo de entrada
+      setContact("");
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>gdfg</Text>
-      <Button
-      style={styles.buttonColor}
-      title="Learn More"
-      accessibilityLabel="Learn more about this purple button"
-    />
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Contact information"
+          onChangeText={(text) => setContact(text)}
+          value={contact}
+        />
+        <Button title="Add Contact" onPress={addContactHandler} />
+      </View>
+      <View style={styles.contactsContainer}>
+        <Text style={styles.listContact} >List of Contacts</Text>
+        <FlatList
+          data={contacts}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item }) => (
+            <Text>{item.name}</Text>
+          )}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  buttonColor: {
-    color: "#841584"
-  },
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 80,
+    paddingHorizontal: 16,
+    
   },
+  
+  inputContainer: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    marginBottom: 24,
+    justifyContent: "space-evenly",
+    borderBottomWidth: 1,
+
+    borderBottomColor: "#cccccc",
+    
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%",
+    marginRight: 10,
+    padding: 10,
+  },
+  contactsContainer: {
+    flex: 5,
+    alignItems: "center",
+    
+  },
+  listContact: {
+    paddingBottom:20
+  }
 });
+
