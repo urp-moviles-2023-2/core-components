f7afe866cc76a0ca694160c3c604a0ef851586d6
RenzoURP <137726724+RenzoURP@users.noreply.github.com>
2023-09-26 14:19:17 +0000

    No sé qué cambio es

--- App.js
+++ App.js
@@ -1,57 +1,158 @@
-import { StyleSheet, Text, TextInput, View, Button } from "react-native";
-import React from "react";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button, Alert, TextInput, SafeAreaView, VirtualizedList, FlatList, Pressable,Modal } from 'react-native';
+import React, { Component } from 'react';
+
+
+
+
+
 
 export default function App() {
-  const [contact, setContact] = React.useState("");
 
-  //TODO: Tip 1 for the assignment 2
-  //const [contacts, setContacts] = React.useState([]);
+  const [contact, setContacto] = React.useState('');
+  const [lista_contactos, setListaContactos] = React.useState([]);
+  const [modalVisible, setModalVisible] = React.useState(false);
 
-  const addContactHandler = () => {
-    console.log(contact);
+  function contactItemHandler(item) {
+    console.log(item)
+    console.log("Pressed")
+  }
 
-    //TODO: Tip 2 for the assignment 2
-    //setContacts([...contacts, { name: contact }]);
+  //* Renderiza cada item de la lista
+  const renderItem = ({ item }) => (
+    <Pressable onPress={() => setModalVisible(true)}>
+      <View>
+        <Text>{item.nombre}</Text>
+      </View>
+    </Pressable>
+  );
+
+  //*Función para añadir contactos a la lista de contactos
+  const añadir_contacto = () => {
+    //* Verifica si se ingresó un contacto y lo agrega a la lista
+    if (contact) {
+      const newContact = { nombre: contact };
+      setListaContactos([...lista_contactos, newContact]);
+      setContacto("");
+    }
   };
+
+
+
   return (
-    <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
+    //*View de la app
+    <SafeAreaView style={styles.container}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        style = {styles.modalView}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+          setModalVisible(!modalVisible);
+        }}>
+        <View >
+          <View >
+            <Text >Hello World!</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}>
+              <Text>Hide Modal</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      <View style={styles.input_container}>
+        <Text>Contacto</Text>
         <TextInput
-          style={styles.textInput}
-          placeholder="Contact information"
-          onChangeText={(text) => setContact(text)}
+          style={styles.input}
+          placeholder='Ingresar Contacto'
+          onChangeText={(contacto) => setContacto(contacto)}
+          value={contact}
         />
-        <Button title="Add Contact" onPress={addContactHandler} />
+        <Button
+          title="Agregar Contacto"
+          onPress={añadir_contacto}
+        />
+
       </View>
-      <View style={styles.contactsContainer}>
-        <Text>List of Contacts...</Text>
+      <View style={styles.contacts_container}>
+        <Text> Lista de Contactos</Text>
+
+
+        <FlatList
+          data={lista_contactos}
+          renderItem={renderItem}
+          keyExtractor={(contacto, index) => index.toString()}
+        />
       </View>
-    </View>
+
+
+
+
+      <StatusBar style="auto" />
+
+    </SafeAreaView>
   );
 }
 
+
+//* Estilos para la app
 const styles = StyleSheet.create({
-  appContainer: {
-    flex: 1,
-    padding: 80,
-    paddingHorizontal: 16,
+
+
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
   },
-  inputContainer: {
+  text_contacto: {
+    paddingBottom: 30
+  },
+  input: {
+    height: 40,
+    width: 190,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10
+
+  },
+  button: {
+    height: 30,
+    color: "#fff",
+  },
+  input_container: {
     flex: 1,
     flexDirection: "column",
-    alignItems: "center",
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: "#cccccc",
+    width: '100%',
+    backgroundColor: "#8CAE68",
+    justifyContent: "center",
+    alignItems: 'center',
+    marginTop: 50,
+    padding: 0
   },
-  textInput: {
-    borderWidth: 1,
-    borderColor: "#cccccc",
-    width: "70%",
-    marginRight: 10,
-    padding: 10,
+  contacts_container: {
+    flex: 2,
+    marginTop: 20,
+    width: '100%',
+    justifyContent: "center",
+    alignItems: 'center',
+    marginBottom: 20
   },
-  contactsContainer: {
-    flex: 6,
+  container: {
+    flex: 1,
+    backgroundColor: '#DFE0E2',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
